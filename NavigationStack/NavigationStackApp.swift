06691d112fd45e64c188628d983871bd22a31b1c
Charlotte <charlotteehwang@gmail.com>
2025-07-24 09:57:53 -0700

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Charlotte Hwang on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
