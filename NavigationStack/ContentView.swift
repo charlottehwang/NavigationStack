1f3ba4614c16ad9f97c424521945c451f2fee2d7
Charlotte <charlotteehwang@gmail.com>
2025-07-24 14:48:48 -0700

    final result of project with three views and colored backgrounds.

--- NavigationStack/ContentView.swift
+++ NavigationStack/ContentView.swift
@@ -10,21 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("This is the root view!")
-                    .font(.title3)
-                    .padding()
-                NavigationLink(destination: ThirdView()){
-                    Text("Playful Cat!")
-                }
-                NavigationLink(destination: SecondView()) {
-                    Text("Burrito Cat!")
+            ZStack{
+                Color(.systemYellow)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("This is the root view!")
+                        .font(.title3)
+                        .padding()
+                    NavigationLink(destination: ThirdView()){
+                        Text("Playful Cat!")
+                    }
+                    NavigationLink(destination: SecondView()) {
+                        Text("Burrito Cat!")
+                        
+                    }
                     
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationBarHidden(true)
                 }
-                
-                .navigationTitle("Home")
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarHidden(true)
             }
         }
     }
